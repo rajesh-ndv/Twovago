65603919f5c7a3f47997520ff01ef0ec3d3840ee
Valesh <valesh.levin.mathias@sap.com>
2021-12-15 01:18:11 +0530

    Display Popup added, Payment modified

--- src/components/TrainList.vue
+++ src/components/TrainList.vue
@@ -18,7 +18,9 @@
                     <td>{{obj.arrivalDate}}</td>
                     <td>{{obj.departureDate}}</td>
                     <td>{{obj.count}}</td>
-                    <td><button class="btn btn-outline-primary" to="\">E</button></td>
+                    <td>
+                        <a v-on:click="alertDisplay(obj.trainName, obj.trainNo, obj.source, obj.destination, obj.arrivalDate, obj.departureDate, obj.count)">view details</a>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -29,6 +31,34 @@
     export default {
         created(){
             console.log(this.$route.params.trains);
+        },
+        methods: {
+            alertDisplay(train_name, train_no, src, dest, arr, dept, count) {
+                this.$swal({
+                    title: "Train Details",
+                    html:
+                        "<h1>Hello</h1>",
+                    text: 
+                        "Train Name : " + train_name + "\n" +
+                        "Train Number : " + train_no + "\n" +
+                        "Source : " + src + "\n" +
+                        "Destination : " + dest + "\n" +
+                        "Arrival Time : " + arr + "\n" +
+                        "Departure Time : " + dept + "\n" +
+                        "Tickets : " + count + "\n",
+                    showCloseButton: true,
+                    showCancelButton: true,
+                    showLoaderOnConfirm: true,
+                    buttons: {
+                        cancel: true,
+                        confirm: "Proceed to Payment",
+                    },
+                }).then(function(res) {
+                    if (res) {
+                        window.location = "/payment-details"
+                    }
+                })
+            }
         },
         data() {
             return {data: this.$route.params.trains}
